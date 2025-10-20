c3c459d347721c41fc3352b65456d15f806edf93
ijustsea <57539185+ijustsea@users.noreply.github.com>
2025-10-20 09:16:58 +0900

    dd
    dd

--- section14/src/App.jsx
+++ section14/src/App.jsx
@@ -4,19 +4,99 @@ import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
 import Notfound from "./pages/Notfound";
 import New from "./pages/New";
+
 import { Routes, Route } from "react-router-dom";
+import { useReducer, useRef, createContext } from "react";
+
+const mockData = [
+  {
+    id: 1,
+    createDate: new Date("2025-10-17").getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용입니다.",
+  },
+  {
+    id: 2,
+    createDate: new Date("2025-10-16").getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용입니다.",
+  },
+  {
+    id: 3,
+    createDate: new Date("2025-9-17").getTime(),
+    emotionId: 3,
+    content: "3번 일기 내용입니다.",
+  },
+];
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [...state, action.data];
+    case "UPDATE":
+      return state.map((item) =>
+        String(item.id) === String(action.data.id) ? action.data : item
+      );
+    case "DELETE":
+      return state.filter((item) => String(item.id) !== String(action.data.id));
+    default:
+      return state;
+  }
+}
+
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
 
 function App() {
+  const [data, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(mockData.length + 1);
+
+  const onCreate = (createDate, emotionId, content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        createDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  const onUpdate = (id, createDate, emotionId, content) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id,
+        createDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  const onDelete = (id) => {
+    dispatch({
+      type: "DELETE",
+      data: {
+        id,
+      },
+    });
+  };
+
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/edit/:id" element={<Edit />} />
-        <Route path="/diary/:id" element={<Diary />} />
-        <Route path="*" element={<Notfound />} />
-        <Route path="/new" element={<New />} />
-      </Routes>
-      테스트
+      <DiaryStateContext.Provider value={data}>
+        <DiaryDispatchContext.Provider value={{ onCreate, onUpdate, onDelete }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/edit/:id" element={<Edit />} />
+            <Route path="/diary/:id" element={<Diary />} />
+            <Route path="*" element={<Notfound />} />
+            <Route path="/new" element={<New />} />
+          </Routes>
+        </DiaryDispatchContext.Provider>
+      </DiaryStateContext.Provider>
     </>
   );
 }
--- section14/src/components/DiaryList.jsx
+++ section14/src/components/DiaryList.jsx
@@ -1,12 +1,11 @@
 import "./DiaryList.css";
 import Button from "./Button";
-import DiaryItem from "./DiaryItem.JSX";
+import DiaryItem from "./DiaryItem";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 const DiaryList = ({ data }) => {
   const nav = useNavigate();
-
   const [sortType, setSortType] = useState("latest");
 
   const onChangeSortType = (e) => {
@@ -23,19 +22,19 @@ const DiaryList = ({ data }) => {
     });
   };
 
-  const sortedData = getSortedData(); //정렬된 데이터
+  const sortedData = getSortedData();
 
   return (
     <div className="DiaryList">
       <div className="menu_bar">
-        <select onChange={onChangeSortType} name="" id="">
+        <select name="" id="">
           <option value="latest">최신순</option>
           <option value="oldest">오래된순</option>
         </select>
         <Button
           onClick={() => nav("/new")}
           text={"새 일기 쓰기"}
-          type={"POSITIVE"}
+          type={"=POSITIVE"}
         />
       </div>
       <div className="list_wrapper">
@@ -46,4 +45,5 @@ const DiaryList = ({ data }) => {
     </div>
   );
 };
+
 export default DiaryList;
